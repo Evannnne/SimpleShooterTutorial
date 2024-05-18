1b9d3d7882769e7e5710c04261733baeaf82d3a1
Evan Sarkozi <prefsuite@Live.ca>
2024-05-18 14:37:45 -0700

    Tutorial start

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -5,31 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    private NavMeshAgent m_agent;
-    private PlayerBehaviour m_player;
-
-    public float health = 100;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_agent = GetComponent<NavMeshAgent>();
-        m_player = FindObjectOfType<PlayerBehaviour>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        m_agent.SetDestination(m_player.transform.position);
-    }
-
-    public void Hit(float damage)
-    {
-        FlashRed();
-        health -= damage;
-        if (health <= 0) Destroy(gameObject);
-    }
-
+    // @ TODO: Implement Enemy
 
     #region Pre-Included Code
     private Coroutine m_currentFlashCoroutine;
